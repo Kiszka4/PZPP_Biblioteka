6ace6132a2905a1715746993bed399a2d8710cfd
Venatrix <daria2002hebda@gmail.com>
2026-05-11 12:14:14 +0200

    feat: add branch management window code-behind

---
+++ PZPP_Biblioteka/FiliaWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace PZPP_Biblioteka
+{
+    public partial class FiliaWindow : Window
+    {
+        public FiliaWindow(FiliaViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
