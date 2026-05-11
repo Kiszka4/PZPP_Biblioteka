da073b36aaeca3627ab1aa5a0b426c67aa14d9e6
Venatrix <daria2002hebda@gmail.com>
2026-05-11 12:14:42 +0200

    feat: add add-branch window code-behind

---
+++ PZPP_Biblioteka/FilialDodajWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PZPP_Biblioteka
+{
+    public partial class FilialDodajWindow : Window
+    {
+        public FilialDodajWindow(Biblioteka context)
+        {
+            InitializeComponent();
+            var vm = new FiliaZapiszViewModel(context);
+            vm.ZamknijOkno += () => Close();
+            DataContext = vm;
+        }
+    }
+}
