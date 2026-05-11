fc93437bd596c807aff3beeadcf4e8539b7fd422
Venatrix <daria2002hebda@gmail.com>
2026-05-11 12:03:33 +0200

    feat: add Filia model with StanyMagazynowe collection

---
+++ PZPP_Biblioteka/Filia.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PZPP_Biblioteka
+{
+    public class Filia
+    {
+        [Key]
+        public int ID { get; set; }
+        public string? Nazwa { get; set; }
+        public string? Lokalizacja { get; set; }
+
+        public ICollection<StanMagazynowy> StanyMagazynowe { get; set; } = new List<StanMagazynowy>();
+    }
+}
