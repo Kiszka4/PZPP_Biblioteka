daa7da61cc89bcb636643a260f4fcc4218c5f5b5
Venatrix <daria2002hebda@gmail.com>
2026-05-11 12:13:51 +0200

    feat: add edit-branch window code-behind

---
+++ PZPP_Biblioteka/FiliaEdytujWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PZPP_Biblioteka
+{
+    public partial class FiliaEdytujWindow : Window
+    {
+        public FiliaEdytujWindow(Biblioteka context, Filia filia)
+        {
+            InitializeComponent();
+            var vm = new FiliaZapiszViewModel(context, filia);
+            vm.ZamknijOkno += () => Close();
+            DataContext = vm;
+        }
+    }
+}
