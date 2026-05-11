65d3770cd7f0b82927d614f85b25f3142bf4f895
Venatrix <daria2002hebda@gmail.com>
2026-05-11 12:02:35 +0200

    feat: add StanMagazynowy model

---
+++ PZPP_Biblioteka/StanMagazynowy.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PZPP_Biblioteka
+{
+    public class StanMagazynowy
+    {
+        [Key]
+        public int ID { get; set; }
+        public int IloscNaStanie { get; set; }
+
+        public int KsiążkaISBN { get; set; }
+        public Książka Książka { get; set; } = null!;
+
+        public int FiliaID { get; set; }
+        public Filia Filia { get; set; } = null!;
+    }
+}
